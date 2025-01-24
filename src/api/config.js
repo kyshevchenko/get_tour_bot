23a54d0630fcd340cbe47e4fb05bf50f740d7a03
kiril.shevchenko <kiril.shevchenko@x5.ru>
2025-01-25 00:33:04 +0300

    add subscription scenes

---
+++ src/api/config.js
@@ -0,0 +1,81 @@
+const HTTP_METHOD = {
+    GET: "get",
+    POST: "post",
+    PUT: "put",
+    PATCH: "PATCH",
+    DELETE: "delete",
+  };
+  
+  export const getRequest = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+    
+      return data;
+    } catch (error) {
+      console.error("Error in getRequest:", error);
+      throw error;
+    }
+  
+  };
+  
+  export const postRequest = async (url, payload) => {
+    try {
+      const response = await fetch(url, {
+        method: HTTP_METHOD.POST,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+  
+      const data = await response.json();
+    
+      return data;
+    } catch (error) {
+      console.error("Error in postRequest:", error);
+      throw error;
+    }
+  
+  };
+  
+  export const patchRequest = async (url, payload) => {
+    try {
+      const response = await fetch(url, {
+        method: HTTP_METHOD.PATCH,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Failed with status ${response.status}`);
+      }
+  
+      const text = await response.text();
+      return text ? JSON.parse(text) : {};
+    } catch (error) {
+      console.error("Error in patchRequest:", error);
+      throw error;
+    }
+  };
+  
+  export const deleteRequest = async (url, payload) => {
+    try {
+      const response = await fetch(url, {
+        method: HTTP_METHOD.DELETE,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Failed with status ${response.status}`);
+      }
+  
+      const text = await response.text();
+      return text ? JSON.parse(text) : {};
+    } catch (error) {
+      console.error("Error in deleteRequest:", error);
+      throw error;
+    }
+  };
+  
